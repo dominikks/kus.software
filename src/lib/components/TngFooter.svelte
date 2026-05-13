83d58edb9b474b65ec84a25e970587d91a2649af
Dominik Kus <dominik@kdtk.de>
2026-05-13 16:57:03 +0100

    Pull TngFooter out into dedicated component

---
+++ src/lib/components/TngFooter.svelte
@@ -0,0 +1,14 @@
+<p class="tng-footer">
+  <em>Note:</em> This article was originally written during my employment at
+  <a href="https://www.tngtech.com">TNG</a> and is republished here with permission. The opinions are
+  my own and TNG does not endorse this publication.
+</p>
+
+<style lang="scss">
+  @use '../style/variables' as *;
+
+  .tng-footer {
+    @include text-meta(13px, 400);
+    margin-top: var(--space-8);
+  }
+</style>
